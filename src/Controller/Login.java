54187ce9b7eefc98429444a685407c1617ad9e4c
Jeremy <jeremyvarkey@gmail.com>
2022-10-31 19:25:15 -0600

    Created Update for Login Controller including logging in user by comparing username and password in DB.

--- src/Controller/Login.java
+++ src/Controller/Login.java
@@ -1,6 +1,53 @@
 package Controller;
 
+import DAO.GeneralHelper;
+import DAO.LoginHelper;
+import Model.JDBC;
+import com.mysql.cj.log.Log;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class Login {
 
+    Stage stage;
+    Parent scene;
+
+    @FXML private Button exitButton;
+    @FXML private ComboBox<?> language;
+    @FXML private Button loginButton;
+    @FXML private TextField password;
+    @FXML private TextField timezone;
+    @FXML private TextField username;
+
+    @FXML
+    void ExitProgram(ActionEvent event) {
+        JDBC.closeConnection();
+        System.exit(0);
+    }
+
+    @FXML
+    void LoginToProgram(ActionEvent event) throws IOException, SQLException {
+        String user = username.getText();
+        String pass = password.getText();
+
+        if (LoginHelper.getUserPasswordMatch(user, pass)) {
+            stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(getClass().getResource("/View/MainMenu.fxml"));
+            stage.setScene(new Scene(scene));
+            stage.show();
+        } else {
+            GeneralHelper.createErrorMessage("Incorrect Username or Password. Please try again.");
+        }
+    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.sql.SQLException;
+import java.time.ZoneId;
 
 
 public class Main extends Application {
