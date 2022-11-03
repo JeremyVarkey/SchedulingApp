acb467041b606575e5196b8681b442a33fe0d69f
Jeremy <jeremyvarkey@gmail.com>
2022-11-02 20:44:27 -0600

    Created Update for Login Controller including logging in user by comparing username and password in DB.

--- src/Controller/Login.java
+++ src/Controller/Login.java
@@ -7,6 +7,7 @@ import com.mysql.cj.log.Log;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -15,15 +16,19 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
+import java.time.ZoneId;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class Login {
+public class Login implements Initializable {
 
     Stage stage;
     Parent scene;
 
     @FXML private Button exitButton;
-    @FXML private ComboBox<?> language;
+    @FXML private TextField language;
     @FXML private Button loginButton;
     @FXML private TextField password;
     @FXML private TextField timezone;
@@ -44,10 +49,22 @@ public class Login {
             stage = (Stage)((Button)event.getSource()).getScene().getWindow();
             scene = FXMLLoader.load(getClass().getResource("/View/MainMenu.fxml"));
             stage.setScene(new Scene(scene));
+            stage.setTitle("Main Menu");
             stage.show();
         } else {
             GeneralHelper.createErrorMessage("Incorrect Username or Password. Please try again.");
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resource) {
+        Locale locale = Locale.getDefault();
+        Locale.setDefault(locale);
+        ZoneId location = ZoneId.systemDefault();
+
+        timezone.setText(location.toString());
+
+
     }
 
 }
